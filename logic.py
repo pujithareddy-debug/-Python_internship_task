ec2f2ae3fb158d0458eae45720a9d9465820f00c
pujithareddy-debug <pallepallepujithareddy@gmail.com>
2026-05-20 18:39:14 +0530

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,9 @@
+username = "pujithareddy"
+password = "1234"
+user = input("Enter Username: ")
+pwd = input("Enter Password: ")
+if user == username and pwd == password:
+    print("Login Successful")
+else:
+    print("Invalid Username or Password")
+

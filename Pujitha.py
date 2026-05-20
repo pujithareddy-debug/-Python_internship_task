57f04a1e751e843501b330a42e5a1b21c886d3e0
pujithareddy-debug <pallepallepujithareddy@gmail.com>
2026-05-20 18:05:57 +0530

    Add files via upload

---
+++ Pujitha.py
@@ -0,0 +1,10 @@
+name = "Pujitha"
+roll_no = 123
+college = "GPCET"
+branch = "ECE"
+cgpa = 8.5
+print("Student name",name)
+print("Student roll no",roll_no)
+print("Student college name",college)
+print("Student branch",branch)
+print("Student cgpa",cgpa)

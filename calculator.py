4f0c76814bdd22ccae57e1727de68a036ccc1cc5
pujithareddy-debug <pallepallepujithareddy@gmail.com>
2026-05-20 18:30:20 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("\nArithmetic Operations")
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("\nAssignment Operations")
+a += b
+print("After a += b :", a)
+a -= b
+print("After a -= b :", a)
+a *= b
+print("After a *= b :", a)
+a /= b
+print("After a /= b :", a)

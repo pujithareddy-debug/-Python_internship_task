8330055177f14752f2fa8dd3a33fa3de4ef257da
pujithareddy-debug <pallepallepujithareddy@gmail.com>
2026-05-21 18:49:53 +0530

    Enhance student details with attendance and marks
    Added lab attendance and internal marks to student details.

--- Pujitha.py
+++ Pujitha.py
@@ -1,9 +1,13 @@
 name = "Pujitha"
+lab_attendance = 90
+internal_marks = 85
 roll_no = 123
 college = "GPCET"
 branch = "ECE"
 cgpa = 8.5
 print("Student name",name)
+print("Student lab attendance",lab_attendance)
+print("Student internal marks",internal_marks)
 print("Student roll no",roll_no)
 print("Student college name",college)
 print("Student branch",branch)

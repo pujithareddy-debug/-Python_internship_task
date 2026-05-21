2ab9b31bef79c3f2eb9a80917020f585539b6197
pujithareddy-debug <pallepallepujithareddy@gmail.com>
2026-05-21 18:59:36 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,6 +5,8 @@ print("Addition =", a + b)
 print("Subtraction =", a - b)
 print("Multiplication =", a * b)
 print("Division =", a / b)
+print("Exponent:", a **b)
+print("Floor Division:", a //b)
 print("\nAssignment Operations")
 a += b
 print("After a += b :", a)
@@ -14,3 +16,9 @@ a *= b
 print("After a *= b :", a)
 a /= b
 print("After a /= b :", a)
+a%= b
+print("After a %= b :", a)
+a **= b
+print("After a **= b :", a)
+a //= b
+print("After a //= b :", a)

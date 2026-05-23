3080723dc4094b12cbb098d6f7f7fb5a1e45ed97
pujithareddy-debug <pallepallepujithareddy@gmail.com>
2026-05-23 12:38:40 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,9 +1,11 @@
-username = "pujithareddy"
-password = "1234"
-user = input("Enter Username: ")
+user = input("enter your username ")
+password = input("enter your password")
+print("your user and password are created")
+username = user
+password_1 = password 
+user_name= input("Enter Username: ")
 pwd = input("Enter Password: ")
-if user == username and pwd == password:
+if user_name == username and pwd == password_1:
     print("Login Successful")
 else:
     print("Invalid Username or Password")
-
